828877e08d5db5a0553105cc5339bd10419daf10
Unik Dahal <61407386+unikdahal@users.noreply.github.com>
2022-10-16 23:49:15 +0530

    Time: 7 ms (45.67%), Space: 7.9 MB (38.98%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void solve(vector<int> nums,vector<vector<int>> &ans,int index){
+        
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
+            solve(nums,ans,index+1);
+            swap(nums[i],nums[index]);
+        }
+        
+        
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        
+        
+        int i=0;
+        vector<vector<int>> ans;
+        int index=0;
+        
+        solve(nums,ans,index);
+        return ans;
+    }
+};
